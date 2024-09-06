240a440c81c1a414d9381e51d60684e666f805e9
Evgen Bratenkov <giertrut@mail.ru>
2024-09-06 19:19:47 +0300

    added model analysisresult

---
+++ app/models/analysisresult.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+
+from app.backend.database import Base
+from app.models import *
+
+
+class AnalysisResult(Base):
+    __tablename__ = 'analysis_results'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    audio_diary_id = Column(Integer, ForeignKey('audio_diaries.id'))
+    mood = Column(String)                                  # Определение настроения (радость, грусть, тревога и т.д.)
+    stress_level = Column(Integer)                         # Уровень стресса
+    key_insights = Column(Text)                            # Ключевые выводы из анализа
+
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    # Связи
+    user = relationship('User', back_populates='analysis_results')
+    audio_diary = relationship('AudioDiary', back_populates='analysis_result')
